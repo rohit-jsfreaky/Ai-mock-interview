c920a2a2b27d64f7ae516f9ae907209774a61200
Rohit <rohitkashyapmrt@gmail.com>
2025-03-08 11:29:54 +0530

    gemini model changed

--- utils/GeminiAiModal.js
+++ utils/GeminiAiModal.js
@@ -4,7 +4,7 @@ const apiKey = process.env.NEXT_PUBLIC_GEMINI_API_KEY;
 const genAI = new GoogleGenerativeAI(apiKey);
 
 const model = genAI.getGenerativeModel({
-  model: "gemini-exp-1114",
+  model: "gemini-2.0-flash",
 });
 
 const generationConfig = {
